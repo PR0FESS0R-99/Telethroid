15ab5a063d0a9969e0aba3e5e1d2a636666820d2
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-21 21:06:45 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import re
+import setuptools
+
+with open('requirements.txt') as fp:
+    requirements = [line.strip() for line in fp]
+
+with open('PyClient/__init__.py') as fp:
+    version = re.search('__version__ = "(.+?)"', fp.read())[1]
+
+setuptools.setup(
+    name="PyClient",
+    version=version,
+    author="Muhammed RK",
+    author_email="muhammedmotech@gmail.com",
+    license="MLT",
+    description="Symbol Programming Language",
+    long_description_content_type="text/markdown",
+    url="https://github.com/PR0FESS0R-99/PyClient",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='~=3.7',
+    py_modules=["PyClient"],
+    install_requires=requirements,
+)
