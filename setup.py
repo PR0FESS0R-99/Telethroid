7e0881bc4b65a3b395102ec969ac72fb5c3b7ea8
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-22 10:40:15 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setuptools.setup(
       name='PyroPath',
       version='0.0.1',
       author='Muhammed',
-      description='Just a simple Python package. Created By @Pr0fess0r_99',                           
       license='MIT License',
+      description='Just a simple Python package. Created By @Pr0fess0r_99',                           
+      package_data={
+        "pyrogram": ["py.typed"],
+      },
+      zip_safe=False,
       packages=['PyroPath'],
 )
