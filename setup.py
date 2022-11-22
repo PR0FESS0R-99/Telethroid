a251271635e000218899d4971dc1716dde79e1f8
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-11-22 10:50:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 import setuptools  
 
 setuptools.setup(
-      name='PyroPath',
-      version='0.0.1',
-      author='Muhammed',
-      license='MIT License',
-      description='Just a simple Python package. Created By @Pr0fess0r_99',                           
-      package_data={
-        "PyroPath",
-      },
-      url="https://github.com/Pr0fess0r-99",
-      download_url="https://github.com/Pr0fess0r-99/PyroPath", 
-      packages=['PyroPath'],
+    name='PyroPath',
+    version='0.0.1',
+    author='Muhammed',
+    license='MIT License',
+    description='Just a simple Python package. Created By @Pr0fess0r_99',                           
+    package_data={
+      "PyroPath",
+    },
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='~=3.7',
+    py_modules=["PyroPath"],
 )
