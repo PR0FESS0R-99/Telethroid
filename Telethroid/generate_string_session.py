4246fc8d401bfd8ec7467021b258e08e36182e64
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-06 16:21:23 +0000

    `generate_string_session` fixed

--- Telethroid/generate_string_session.py
+++ Telethroid/generate_string_session.py
@@ -1,26 +1,19 @@
-import asyncio
+async def generate_string_session(pyrogram, api_id=False, api_hash=False, mode="PHONE_NUMBER"): # PHONE_NUMBER or BOT_TOKEN
+  if api_id & api_hash == False:
+    api_id = input("Enter your teegram API_ID")
+    api_hash = input("Okke, Enter your teegram API_HASH")
 
-def generate_string_session(pyrogram, api_id=False, api_hash=False, mode="PHONE_NUMBER"): # PHONE_NUMBER or BOT_TOKEN
-  def main():
-    
-    if api_id & api_hash == False:
-      api_id = input("Enter your teegram API_ID")
-      api_hash = input("Okke, Enter your teegram API_HASH")
-    
-    try:
-      app = pyrogram.Client("my_account", api_id=int(api_id), api_hash=api_hash)
-    except Exception as e:
-      print(f"Error {e}")
-      return
+  try:
+    app = pyrogram.Client("my_account", api_id=int(api_id), api_hash=api_hash)
+  except Exception as e:
+    print(f"Error {e}")
+    return
 
-    with app:
-      s = await app.export_session_string()
-
-      if out_put == "PHONE_NUMBER":
-        caption = (f"Pyro String Session v{pyrogram.__version__}! \n\n<code>{s}</code>")
-        await app.send_message(chat_id="me", text=caption)
-      elif out_put == "BOT_TOKEN":
-        caption = (f"Pyro String Session v{pyrogram.__version__}! \n\n<code>{s}</code>")
-        print(caption)
-
-  app.run(main())
+  async with app:
+    s = await app.export_session_string()
+    if out_put == "PHONE_NUMBER":
+      caption = (f"Pyro String Session v{pyrogram.__version__}! \n\n<code>{s}</code>")
+      await app.send_message(chat_id="me", text=caption)
+    elif out_put == "BOT_TOKEN":
+      caption = (f"Pyro String Session v{pyrogram.__version__}! \n\n<code>{s}</code>")
+      print(caption)
