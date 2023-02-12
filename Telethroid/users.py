b61d670324aeb6a758f13dff07ad687232e60525
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-12 12:14:41 +0000

    anonymous

--- Telethroid/users.py
+++ Telethroid/users.py
@@ -2,19 +2,19 @@ def anonymous_id(message):
   return message.from_user.id if message.from_user else None
 
 def anonymous_first_name(message):
-  return message.from_user.first_name if message.from_user. else None
+  return message.from_user.first_name if message.from_user else None
 
 def anonymous_last_name(message):
-  return message.from_user.last_name  if message.from_user. else None
+  return message.from_user.last_name  if message.from_user else None
 
 def anonymous_username (message):
-  return message.from_user.username  if message.from_user. else None
+  return message.from_user.username  if message.from_user else None
 
 def anonymous_language_codemessage):
-  return message.from_user.language_code if message.from_user. else None
+  return message.from_user.language_code if message.from_user else None
 
 def anonymous_dc_id(message):
-  return message.from_user.dc_id if message.from_user. else None
+  return message.from_user.dc_id if message.from_user else None
 
 def anonymous_mention(message):
-  return message.from_user.mention if message.from_user. else None
+  return message.from_user.mention if message.from_user else None
