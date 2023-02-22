c9b8d904f2a23e4db5bb42504d7adaa1c433d838
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-22 23:06:53 +0530

    Update __init__.py

--- Telethroid/__init__.py
+++ Telethroid/__init__.py
@@ -5,7 +5,7 @@ __version__ = "0.0.9"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2023-present Pr0fess0r-99 <https://github.com/Pr0fess0r-99>"
 
-def start_telethroid():
+def started_telethroid():
   print(
     "Telethroid Successfully Installed" + "\n"
     f"Version : {__version__}"
