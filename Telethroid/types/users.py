4dd26ae2d0a935b33e10794219bc77827fc48a59
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-22 22:39:21 +0530

    Update users.py

--- Telethroid/types/users.py
+++ Telethroid/types/users.py
@@ -1,21 +1,54 @@
+from .mode import Mode
+
 class User(object):
-    def id(message):
-        return message.from_user.id
+    def id(update):
+        return update.from_user.id
 
-    def first(message):
-      return message.from_user.first_name
+    def first(update):
+      return update.from_user.first_name
 
-    def last(message):
-      return message.from_user.last_name
+    def last(update):
+      return update.from_user.last_name
 
-    def username(message):
-      return message.from_user.username
+    def username(update):
+      return update.from_user.username
 
-    def lang_code(message):
-      return message.from_user.language_code
+    def lang_code(update):
+      return update.from_user.language_code
 
-    def dc_id(message):
-      return message.from_user.dc_id
+    def dc_id(update):
+      return update.from_user.dc_id
 
-    def mention(message):
-      return message.from_user.mention
+    def mention(update):
+      return update.from_user.mention
+
+def user_info(update, out_put=Mode.TEXT_MODE):
+
+  if out_put == Mode.TEXT_MODE:
+      return message = (
+         # users normal details
+         f"• First : {User.first(update)}" + "\n"
+         f"• Last : {User.last(update)}" + "\n"
+         f"• ID : {User.id(update)}" + "\n"
+         f"• Username : {None if not User.username(update) else '@' + User.username(update)}" + "\n"
+         f"• Mention : {User.mention(update)}"
+      )  
+  elif out_put == Mode.DATA_MODE:
+      return {
+         'first': User.first(update), 
+         'last': User.last(update),
+         'id': User.id(update),
+         'username': User.username(update),
+         'mention': User.mention(update)
+      }
+  elif out_put == Mode.NUMBER_MODE:
+      return [
+         'user',
+         User.first(update),
+         User.last(update),
+         User.id(update),
+         User.username(update),
+         User.mention(update)
+      ]
+  else:
+      print(f"Data Invalid : {out_put}")     
