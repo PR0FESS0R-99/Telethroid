8a615801204c9a9dbc256c824a06caa520bb7767
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-22 18:02:23 +0530

    .

--- Telethroid/errors/anonymous.py
+++ Telethroid/errors/anonymous.py
@@ -1,21 +1,21 @@
 class Anonymous(object):
-    def ID(message):
+    def id(message):
         return message.from_user.id if message.from_user else None
 
-    def FIRST(message):
+    def first(message):
       return message.from_user.first_name if message.from_user else None
 
-    def LAST(message):
+    def last(message):
       return message.from_user.last_name  if message.from_user else None
 
-    def USERNAME(message):
+    def username(message):
       return message.from_user.username  if message.from_user else None
 
-    def LANG_CODE(message):
+    def lang_code(message):
       return message.from_user.language_code if message.from_user else None
 
-    def DC_ID(message):
+    def dc_id(message):
       return message.from_user.dc_id if message.from_user else None
 
-    def MENTION(message):
+    def mention(message):
       return message.from_user.mention if message.from_user else None
