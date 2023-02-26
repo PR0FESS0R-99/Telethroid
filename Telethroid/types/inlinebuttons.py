2d8004afe9e5591e53410b8ba4b2b366c3163a12
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-26 16:49:58 +0530

    `InlineButtons`
    keyboard = InlineButtons()
    keyboard.add_button('Open Google', url='https://www.google.com')
    keyboard.add_button('Send callback', callback_data='callback_data')
    keyboard.add_row([
        {'text': 'Button 1', 'callback_data': 'button1'},
        {'text': 'Button 2', 'callback_data': 'button2'},
        {'text': 'Button 3', 'callback_data': 'button3'}
    ])
    keyboard_dict = keyboard.to_dict()

---
+++ Telethroid/types/inlinebuttons.py
@@ -0,0 +1,26 @@
+from typing import List, Union
+
+class InlineButtons:
+    def __init__(self) -> None:
+        self.buttons: List[List[dict]] = []
+        
+    def add_button(self, text: str, url: str = None, callback_data: str = None) -> None:
+        button: dict = {
+            'text': text,
+            'url': url,
+            'callback_data': callback_data,
+        }
+        self.buttons.append([button])
+        
+    def add_row(self, row_buttons: List[dict]) -> None:
+        row: List[dict] = []
+        for button in row_buttons:
+            row.append({
+                'text': button['text'],
+                'url': button.get('url'),
+                'callback_data': button.get('callback_data'),
+            })
+        self.buttons.append(row)
+        
+    def to_dict(self) -> dict:
+        return {'inline_keyboard': self.buttons}
