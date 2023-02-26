7ad267163a0a961444b3cc243c3022d9efc1bd79
Szrosebot <114131911+Szrosebot@users.noreply.github.com>
2023-02-26 19:36:31 +0530

    Create sendotp.py

---
+++ Telethroid/types/sendotp.py
@@ -0,0 +1,20 @@
+import random
+from Telethroid import TelethroidClient
+
+class SENDOTP:
+    def __init__(self, telethroid_client: TelethroidClient):
+        self.telethroid_client = telethroid_client
+    
+    def generate_otp(self):
+        return str(random.randint(100000, 999999))
+    
+    def send_otp(self, phone_number: str):
+        otp = self.generate_otp()
+        message = f"Your OTP is {otp}."
+        
+        params = {'phone_number': phone_number, 'message': message}
+        result = self.telethroid_client('send_otp', params)
+        if result.get('status') == 'ok':
+            return otp
+        else:
+            return False
