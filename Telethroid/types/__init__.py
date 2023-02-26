496546890f832c2346bfc8c802ecee7c6574a2cc
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-26 20:37:35 +0530

    Update __init__.py

--- Telethroid/types/__init__.py
+++ Telethroid/types/__init__.py
@@ -1,6 +1,2 @@
-from .admins import Admins
-from .chats import Chats
-from .users import User
-from .mode import Mode
 from .inlinebuttons import InlineButtons
 from .reply_markup import ReplyMarkup
