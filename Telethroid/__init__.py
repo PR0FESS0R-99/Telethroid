bb038674168c187423132d5c5a0fecdd6f4d06d9
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-26 16:15:53 +0530

    `v0.0.10`

--- Telethroid/__init__.py
+++ Telethroid/__init__.py
@@ -1,9 +1,13 @@
 #  Telethroid - Telegram BoT Library for Python
 #  Copyright (C) 2023-present Pr0fess0r-99 <https://github.com/Pr0fess0r-99>
 
-__version__ = "0.0.9"
+__version__ = "0.0.10"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2023-present Pr0fess0r-99 <https://github.com/Pr0fess0r-99>"
+
+import Telethroid.filters
+import Telethroid.types
+import Telethroid.errors
 
 def started_telethroid():
   print(
