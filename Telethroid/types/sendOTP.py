6d53bf259f8be44e22e9cf34bce3582a5fa949fa
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-02-28 18:34:14 +0530

    Client Fixed

--- Telethroid/types/sendOTP.py
+++ Telethroid/types/sendOTP.py
@@ -1,5 +1,5 @@
 import random
-from Telethroid.clients import TelethroidClient
+from Telethroid.__main__ import TelethroidClient
 
 class Send_OTP:
     def __init__(self, telethroid_client: TelethroidClient):
