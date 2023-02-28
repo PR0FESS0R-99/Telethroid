6b4b6adb182d9f47f7145b2029d0cc6941f6b45b
Szrosebot <114131911+Szrosebot@users.noreply.github.com>
2023-02-28 15:35:05 +0530

    Update clients.py

--- Telethroid/clients.py
+++ Telethroid/clients.py
@@ -5,98 +5,114 @@ import threading
 import base64
 import hashlib
 import hmac
-import json
-import time
 import urllib.parse
 import urllib.request
 import platform
+import requests
+import json
+import time
+from Telethroid.types import Filters, Msg
 
-class TelethroidClientTool(object):
-    DEVICE_MODEL = f"{platform.python_implementation()} {platform.python_version()}"
 
 class TelethroidClient:
-    "Telethroid Client"
+    """
+    A client class for interacting with the Telegram Bot API.
 
-    def __init__(
-        self,
-        api_id: int = None,
-        api_hash: str = None,
-        bot_token: str = None,
-        string_session: str = None,
-        phone_number: str = None,
-        device_model: str= TelethroidClientTool.DEVICE_MODEL
-    ):
+    Parameters:
+        token (str): The Telegram bot token.
+        api_id (int): The Telegram API ID for your app.
+        api_hash (str): The Telegram API hash for your app.
+        session (str): The string session for the userbot client (optional).
 
+    Methods:
+        send_message(chat_id: int, text: str) -> dict:
+            Sends a message to the specified chat.
+
+        get_updates() -> list:
+            Retrieves new updates from the Telegram API.
+
+        start_polling(handler) -> None:
+            Continuously polls the Telegram API for updates and calls a handler function for each update.
+
+        run() -> None:
+            Starts the bot client.
+    """
+
+    def __init__(self, token: str, api_id: int, api_hash: str, session: str = None):
+        self.token = token
         self.api_id = api_id
         self.api_hash = api_hash
-        self.bot_token = bot_token
-        self.string_session = string_session
-        self.phone_code = phone_number
-        self.device_model = device_model
+        self.base_url = f"https://api.telegram.org/bot{token}/"
+        self.last_update_id = 0
+        self.session = session
 
-        self.lock = threading.Lock()
+    def send_message(self, chat_id: int, text: str) -> dict:
+        """
+        Sends a message to the specified chat.
 
-    def send_request(self, method: str, params: dict, timeout: int = 30):
-        with self.lock:
-            url = f'https://api.telegram.org/bot{self.bot_token}/{method}'
-            data = urllib.parse.urlencode(params).encode('utf-8')
-            headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        Parameters:
+            chat_id (int): The ID of the chat to send the message to.
+            text (str): The text of the message to send.
 
-            request = urllib.request.Request(url, data=data, headers=headers, method='POST')
-            response = urllib.request.urlopen(request, timeout=timeout)
+        Returns:
+            dict: The response from the Telegram API.
+        """
+        url = f"{self.base_url}sendMessage"
+        data = {"chat_id": chat_id, "text": text}
+        response = requests.post(url, data=data)
+        return json.loads(response.content)
 
-            return json.loads(response.read().decode('utf-8'))
+    def get_updates(self) -> list:
+        """
+        Retrieves new updates from the Telegram API.
 
-    def get_auth_key(self):
-        nonce = str(int(time.time()))
-        data = nonce + self.api_id + self.api_hash + self.bot_token
-        return base64.b64encode(hmac.new(self.phone_code.encode('utf-8'), data.encode('utf-8'), hashlib.sha256).digest())
+        Returns:
+            list: A list of updates from the Telegram API.
+        """
+        url = f"{self.base_url}getUpdates"
+        params = {"offset": self.last_update_id + 1, "timeout": 30}
+        response = requests.get(url, params=params)
+        return json.loads(response.content)
 
-    def login(self):
-        auth_key = self.get_auth_key()
-        params = {'phone_code': self.phone_code, 'device_model': self.device_model, 'auth_key': auth_key}
+    def start_polling(self, handler):
+        """
+        Continuously polls the Telegram API for updates and calls a handler function for each update.
 
-        result = self.send_request('auth.login', params)
-        if result.get('status') == 'ok':
-            self.string_session = result.get('string_session')
-            return True
-        else:
-            return False
-
-    def check_auth(self):
-        params = {'string_session': self.string_session}
-        result = self.send_request('auth.check', params)
-        if result.get('status') == 'ok':
-            return True
-        else:
-            return False
-
-    def send_message(self, chat_id: int, text: str):
-        params = {'chat_id': chat_id, 'text': text}
-        result = self.send_request('messages.send', params)
-        if result.get('status') == 'ok':
-            return True
-        else:
-            return False
-
-    def handle_error(self, error: dict):
-        if error.get('error_code') == 401:
-            self.login()
-        else:
-            raise Exception(f'Telegram API error: {error}')
-
-    def __call__(self, method: str, params: dict):
-        params['string_session'] = self.string_session
-
+        Parameters:
+            handler (function): A function that will be called with each new update.
+        """
         while True:
             try:
-                result = self.send_request(method, params)
-                if result.get('status') == 'ok':
-                    return result.get('result')
-                elif result.get('error'):
-                    self.handle_error(result.get('error'))
-                else:
-                    raise Exception('Unexpected response from Telegram API')
+                updates = self.get_updates()
+                if len(updates) > 0:
+                    self.last_update_id = updates[-1]['update_id']
+                    for update in updates:
+                        handler(update)
             except Exception as e:
-                print(f'Telegram API error: {e}')
-                time.sleep(5)
+                print(f"Error: {e}")
+            time.sleep(1)
+
+    def run(self):
+        """
+        Starts the bot client.
+        """
+        if self.session:
+            from Telethroid import TelegramClient
+
+            client = TelegramClient(self.session, self.api_id, self.api_hash)
+            client.start()
+        else:
+            self.start_polling(self.handle_update)
+
+    def handle_update(self, update):
+        """
+        Handles a single update from the Telegram API.
+
+        Parameters:
+            update (dict): A dictionary representing the update.
+        """
+        if "message" in update:
+            message = message.Msg(update["message"])
+            if filters.command("start")(message):
+                self.send_message(
+                    message.chat_id, "Hello, World!")
