a3d282c92a198e93cd523f3dd32ceadfe448cbba
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-03-01 23:17:02 +0530

    Client Changed

--- Telethroid/__init__.py
+++ Telethroid/__init__.py
@@ -5,7 +5,7 @@ __version__ = "0.0.26"
 __license__ = "GNU Lesser General Public License v3.0 (LGPL-3.0)"
 __copyright__ = "Copyright (C) 2023-present Pr0fess0r-99 <https://github.com/Pr0fess0r-99>"
 
-from Telethroid.clients import TelethroidClient
+from Telethroid.client.telethroid import TelethroidClient
 
 def started_telethroid():
   print(
